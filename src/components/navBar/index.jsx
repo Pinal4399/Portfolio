f5b7c9e0b486a8094f4a136a69c31bbef1168e0d
Pinal4399 <114483204+Pinal4399@users.noreply.github.com>
2025-05-22 14:25:56 +0100

    Update index.jsx

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -51,6 +51,7 @@ const Navbar = () => {
                 </div>
                 <ul
                     className={`navbar__container__menu ${toggleIcon ? "active" : ""}`}
+                     onClick={handleToggleIcon}
                 >
                     {
                         data.map((item, key) => (
